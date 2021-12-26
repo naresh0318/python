bd5b9b54592cf602d477187e1bfe37874861745b
naresh0318 <nareshsharma0318@gmail.com>
2021-12-26 10:45:39 +0530

    adding python intro lecture

---
+++ week2/2-2.py
@@ -0,0 +1,12 @@
+print("Hello folks !! ")
+a = 10
+print("the value of a is = ",a)
+b = 34
+print("the value of b is = ",b)
+c = 67
+print("the value of c is = ",c)
+d = a+b+c
+print("the sum of a + b + c = ",d)
+
+j='anna'
+print("type of j is",type(j))
